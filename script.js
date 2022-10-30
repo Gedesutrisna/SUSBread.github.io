8691c2bda53f1548bed7e3ad31dec9da7fa975f5
mangde <89692448+Gedesutrisna@users.noreply.github.com>
2022-10-30 21:40:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ window.onscroll = () =>{
 }
 
 
+const txtElement = ['SUS BREAD'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+  if(count == txtElement.length){
+    count = 0;
+  }
+
+  currentTxt = txtElement[count];
+
+  words = currentTxt.slice(0, ++txtIndex);
+  document.querySelector('.efek-ngetik').textContent = words;
+
+  if(words.length == currentTxt.length){
+    count++;
+    txtIndex = 0;
+  }
+
+  setTimeout(ngetik, 300);
+
+})();
+
+
